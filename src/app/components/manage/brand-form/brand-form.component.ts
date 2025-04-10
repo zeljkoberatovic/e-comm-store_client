34c47c5d7e1aed11e7dccb723710f173ebe0d345
Željko Beratović <zeljkoberatovic084@gmail.com>
2025-04-10 19:10:48 +0200

    feat: Implementiran interface za modele brand i category,kreirane componente brand i brand-form,kreiran brand.service

---
+++ src/app/components/manage/brand-form/brand-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-form',
+  imports: [],
+  templateUrl: './brand-form.component.html',
+  styleUrl: './brand-form.component.scss'
+})
+export class BrandFormComponent {
+
+}
---
+++ src/app/types/category.ts
@@ -0,0 +1,6 @@
+export interface Category {
+    _id?: string;
+    name: string;
+    createdAt?: string; 
+  updatedAt?: string;
+}
