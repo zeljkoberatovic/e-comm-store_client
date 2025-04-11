b5596def0b71132976a3270e909926e0390e9164
Željko Beratović <zeljkoberatovic084@gmail.com>
2025-04-12 00:35:36 +0200

    feat: ✨ Implementiran kompletan CRUD za Brand entitet: - Dodani environment fajlovi i povezani u BrandService - Kreirani BrandsComponent i BrandFormComponent - Postavljene rute za pregled, kreiranje i uređivanje brandova - Dodana integracija sa REST API-jem preko HttpClient-a

--- src/app/components/manage/brand-form/brand-form.component.ts
+++ src/app/components/manage/brand-form/brand-form.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule, Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BrandService } from '../../../services/brand.service';
+
+
+
 
 @Component({
   selector: 'app-brand-form',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    CommonModule
+  ],
   templateUrl: './brand-form.component.html',
   styleUrl: './brand-form.component.scss'
 })
-export class BrandFormComponent {
+export class BrandFormComponent implements OnInit{
+
+  brandService = inject(BrandService);
+  router = inject(Router);
+  location = inject(Location);
+  private route = inject(ActivatedRoute);
+
+  name!: string;
+  isEdit = false;
+  id!: string;
+
+  ngOnInit() {
+    const { id } = this.route.snapshot.params;
+
+    if (id) {
+      this.isEdit = true;
+      this.id = id; 
+      this.brandService.getBrandById(id).subscribe((result: any) => {
+        //console.log(result);
+        this.name = result.name;
+      });
+    }
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  add() {
+    this.brandService.addBrand(this.name).subscribe(() => {
+      alert("Brand added successfully!");
+      this.router.navigateByUrl("/admin/brands");
+    });
+  }
+
+  update() {
+    if (this.id && this.name) {
+      this.brandService.updateBrand(this.id, this.name).subscribe(() => {
+        alert("Brand updated successfully!"); 
+        this.router.navigateByUrl("/admin/brands");
+      });
+    } else {
+      console.error('ID or name is missing. Cannot update.');
+    }
+  }
+
+  
 
 }
--- src/app/components/manage/brands/brands.component.ts
+++ src/app/components/manage/brands/brands.component.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, inject, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CategoryService } from '../../../services/category.service';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { BrandService } from '../../../services/brand.service';
+
+
+
 
 @Component({
   selector: 'app-brands',
-  imports: [],
+  imports: [MatFormFieldModule,
+            MatInputModule,
+            MatTableModule,
+            MatSortModule,
+            MatPaginatorModule,
+            MatButtonModule,
+            RouterLink],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.scss'
 })
 export class BrandsComponent {
+  displayedColumns: string[] = ['id', 'name', 'action'];
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  brandService = inject(BrandService);
+  
+
+  constructor() {
+
+    this.dataSource = new MatTableDataSource([] as any);
+
+  }
+
+  ngOnInit() {
+    this.getServerData();
+  }
+  private getServerData() {
+    this.brandService.getBrands().subscribe((result:any) => {
+      //console.log(result);
+    this.dataSource.data = result; 
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  delete(id: string) {
+    //console.log(id);
+    this.brandService.deleteBrandById(id).subscribe((result: any) => {
+      alert("Brand Deleted!");
+      this.getServerData(); 
+    })
+  }
+  
+
 
 }
