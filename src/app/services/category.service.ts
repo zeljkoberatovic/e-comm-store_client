871c2bc1e360d88a56c7e41c9b4d1cf3baf6a554
Željko Beratović <zeljkoberatovic084@gmail.com>
2025-04-11 13:47:07 +0200

    feat: integracija environment varijable u CategoryService
    - Dodato korišćenje environment konfiguracije za dinamičko postavljanje API URL-a.
    - Uklonjen hardkodirani URL za category endpoint i zamenjen sa nvironment.apiUrl.
    - Omogućeno jednostavno prebacivanje između razvojne i produkcijske verzije API-a pomoću različitih environment fajlova (nvironment.ts i nvironment.development.ts).

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -9,24 +10,27 @@ export class CategoryService {
 
   constructor() { }
 
+  // Definiši osnovni API URL
+  private apiUrl = environment.apiUrl + '/category';
+
   getCategories(){
-    return this.http.get("http://localhost:3000/category");
+    return this.http.get(this.apiUrl);
   }
 
   getCategoryById(id: string) {
-    return this.http.get("http://localhost:3000/category/" + id);
+    return this.http.get(`${this.apiUrl}/${id}`);
   }
 
   addCategory(name: string) {
-    return this.http.post("http://localhost:3000/category", { name: name});
+    return this.http.post(this.apiUrl, { name: name });
   }
 
   updateCategory(id: string, name: string) {
-    return this.http.put("http://localhost:3000/category/" + id, { name: name });
+    return this.http.put(`${this.apiUrl}/${id}`, { name: name });
   }
 
   deleteCategoryById(id: string) {
-    return this.http.delete("http://localhost:3000/category/" + id);
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
 }
