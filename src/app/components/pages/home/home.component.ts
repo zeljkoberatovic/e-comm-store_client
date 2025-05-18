d8e0076b39e1fd56c15dc1db10aa5a755970d459
Željko Beratović <zeljkoberatovic084@gmail.com>
2025-05-18 11:33:42 +0200

    refactor: Implementirana pretraga sa paginacijom za featured i new products sa dugmetom 'View More'

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,18 +1,21 @@
-import { ChangeDetectorRef } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { CustomerService } from '../../../services/customer.service';
 import { Product } from '../../../types/product';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from '../../manage/product-card/product-card.component';
 import { SearchService } from '../../../services/search.service';
 import { ImageSliderComponent } from '../../shared/image-slider/image-slider.component';
 
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, CommonModule, ProductCardComponent, ImageSliderComponent],
+  imports: [
+    MatCardModule,
+    CommonModule,
+    ProductCardComponent,
+    ImageSliderComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -27,6 +30,12 @@ export class HomeComponent {
   newProduct: Product[] = [];
   featuredProduct: Product[] = [];
 
+  displayedFeatured: Product[] = [];
+  displayedNew: Product[] = [];
+
+  featuredVisibleCount = 5;
+  newVisibleCount = 5;
+
   isSearchActive = false;
 
   ngOnInit() {
@@ -34,17 +43,23 @@ export class HomeComponent {
       this.featuredProduct = result;
       this.allProducts = result;
       this.filteredProducts = result;
+      this.displayedFeatured = this.featuredProduct.slice(0, this.featuredVisibleCount);
     });
 
     this.customerService.getNewProducts().subscribe((result) => {
       this.newProduct = result;
+      this.displayedNew = this.newProduct.slice(0, this.newVisibleCount);
     });
 
     this.searchService.searchTerm$.subscribe((term) => {
-      if (this.allProducts) {
+      if (term && term.trim().length > 0) {
+        this.isSearchActive = true;
+        const lowerTerm = term.toLowerCase();
         this.filteredProducts = this.allProducts.filter(product =>
-          product.name.toLowerCase().startsWith(term)
+          product.name.toLowerCase().includes(lowerTerm)
         );
+      } else {
+        this.isSearchActive = false;
       }
     });
   }
@@ -56,6 +71,16 @@ export class HomeComponent {
 
   addToCart(product: Product) {
     console.log('Dodato u korpu:', product.name);
-    // ovdje dodajemo logiku za dodavanje u korpu
+    // Ovdje dodaj logiku za dodavanje u korpu
+  }
+
+  loadMoreFeatured() {
+    this.featuredVisibleCount += 5;
+    this.displayedFeatured = this.featuredProduct.slice(0, this.featuredVisibleCount);
+  }
+
+  loadMoreNew() {
+    this.newVisibleCount += 5;
+    this.displayedNew = this.newProduct.slice(0, this.newVisibleCount);
   }
 }
