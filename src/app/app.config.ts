1aef6b6f3ae6cbc550abb77da9b63d5325708f2a
Željko Beratović <zeljkoberatovic084@gmail.com>
2025-06-13 11:06:09 +0200

    test-auth.interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
-    
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
+
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 };
