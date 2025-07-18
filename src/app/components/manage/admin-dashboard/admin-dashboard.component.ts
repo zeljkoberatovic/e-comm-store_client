270ab76106047ee7f2e6ee8581f8c46705ea04ed
Željko Beratović <zeljkoberatovic084@gmail.com>
2025-07-18 12:05:35 +0200

    Refaktorisana sidebar navigacija sa poboljšanom preglednošću i stilizacijom
    - Zamenjen href sa Angular routerLink za navigaciju unutar SPA
    - Dodata routerLinkActive klasa za isticanje aktivnog linka
    - Dodate SVG ikonice pored stavki menija radi bolje vizuelne jasnoće
    - Primenjeni Tailwind CSS stilovi sa hover efektima i glatkim prelazima
    - Formatiran HTML sa pravilnim uvlačenjem radi lakšeg održavanja koda

--- src/app/components/manage/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/manage/admin-dashboard/admin-dashboard.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
   imports: [ 
     RouterLink, MatButtonModule,
-    RouterOutlet],
+    RouterOutlet, RouterLinkActive],
     
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
